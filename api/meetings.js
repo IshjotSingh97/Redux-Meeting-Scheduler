dae2f3eb891b41c20e7174707433f54b8e45b8b1
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-02-25 01:45:41 +0530

    initial

--- api/meetings.js
+++
@@ -1,9 +0,0 @@
-const getAllMeetings = async () => {
-    const res = await fetch(`http://localhost:3000/meetings`)
-    const data = await res.json()
-    return data
-}
-
-
-
-export { getAllMeetings }
